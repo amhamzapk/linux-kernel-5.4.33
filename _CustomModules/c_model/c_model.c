cac4dfa49ff885eeab2aff60a68a3213475f71a8
Ameer Hamza <amhamza.pk@gmail.com>
2020-07-02 21:02:15 +0500

    temp

--- _CustomModules/c_model/c_model.c
+++ _CustomModules/c_model/c_model.c
@@ -31,7 +31,7 @@ MODULE_VERSION("0.1");
 #define NUM_CPUS 	4
 #define THOUSAND	1000
 #define MILLION		THOUSAND*THOUSAND
-#define NUM_CMDS	8192
+#define NUM_CMDS	MILLION
 
 int cnt_resp = 0;
 
@@ -240,7 +240,7 @@ static int thread_fn(void *unused)
 					case PROCESS_RX:
 					{
 						/* Print Information */
-//						printk(KERN_ALERT "RX Command | Len = %d | CPU = %d\n", skbuff_ptr->len, skbuff_ptr->meta.cpu);
+						printk(KERN_ALERT "RX Command | Len = %d | CPU = %d\n", skbuff_ptr->len, skbuff_ptr->meta.cpu);
 
 						/* Update response flag */
 						skbuff_ptr->meta.response_flag = CASE_NOTIFY_STACK_RX;
@@ -260,7 +260,7 @@ static int thread_fn(void *unused)
 					case PROCESS_TX:
 					{
 						/* Print Information */
-//						printk(KERN_ALERT "TX Command | Len = %d | CPU = %d\n", skbuff_ptr->len, skbuff_ptr->meta.cpu);
+						printk(KERN_ALERT "TX Command | Len = %d | CPU = %d\n", skbuff_ptr->len, skbuff_ptr->meta.cpu);
 
 						/* Update response flag */
 						skbuff_ptr->meta.response_flag = CASE_NOTIFY_STACK_TX;
