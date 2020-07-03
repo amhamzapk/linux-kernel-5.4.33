2709b2f1714f9aec546eb8adf674df010e92908c
Ameer Hamza <amhamza.pk@gmail.com>
2020-07-03 09:53:10 +0500

    temp_commit

--- _CustomModules/c_model/c_model.c
+++ _CustomModules/c_model/c_model.c
@@ -475,7 +475,7 @@ static int __init nic_c_init(void) {
 		flag[i] = 'n';
 	}
 
-	for (i=0; i<1; i++)
+	for (i=1; i<2; i++)
 	{
 		req_thread_per_cpu[i] = kthread_create(request_thread_per_cpu, NULL, "kthread_cpu_req");
 		kthread_bind(req_thread_per_cpu[i], i);
