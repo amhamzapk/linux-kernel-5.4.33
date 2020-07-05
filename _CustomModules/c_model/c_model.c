7ecc174207441786d5f12124984716a49e45c13c
Ameer Hamza <amhamza.pk@gmail.com>
2020-07-06 01:24:40 +0500

    q_mem_1 allocation by ptr

--- _CustomModules/c_model/c_model.c
+++ _CustomModules/c_model/c_model.c
@@ -295,8 +295,10 @@ static int c_model_worker_thread(void *unused) {
 //                        printk(KERN_ALERT "AAAAAAAAAA\n");
                         ++num_responses_push[skbuff_ptr->meta.cpu];// = ++(num_responses_push[skbuff_ptr->meta.cpu]) ;// % NUM_RESPONSE_WRAP;
 
-                        int temp_timeout = 1000000;
-                        while (--temp_timeout);
+//                        int temp_timeout = 1000000;
+//                        while (--temp_timeout);
+                        clflush(&num_responses_push[skbuff_ptr->meta.cpu]);
+
                         barrier();
                         /* Wake up wait queue for the Response thread */
 //                        flag[skbuff_ptr->meta.cpu] = 'y';
