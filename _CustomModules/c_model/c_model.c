a34d9dff1c3215fc06c3b22cbe78f0529bc0d9cc
Ameer Hamza <amhamza.pk@gmail.com>
2020-07-06 17:14:57 +0500

    q_mem_1 allocation by ptr

--- _CustomModules/c_model/c_model.c
+++ _CustomModules/c_model/c_model.c
@@ -182,8 +182,8 @@ static int push_pop_response(struct skbuff_nic_c **skbuff_struct, int cpu, int i
 
 //    	 if (((mem_allocator_push_idx[cpu]) % RESPONSE_QUEUE_SIZE) != ((mem_allocator_pop_idx[cpu] + 1) % RESPONSE_QUEUE_SIZE)) {
     	        /* Allocate the node and increment push_allocator idx */
-//    	        temp_node = (struct queue_ll*) (response_queue_ptr[cpu] + mem_allocator_push_idx[cpu]);
-//    	        mem_allocator_push_idx[cpu] = (mem_allocator_push_idx[cpu] + 1) % RESPONSE_QUEUE_SIZE;
+    	        temp_node = (struct queue_ll*) (response_queue_ptr[cpu] + mem_allocator_push_idx[cpu]);
+    	        mem_allocator_push_idx[cpu] = (mem_allocator_push_idx[cpu] + 1) % RESPONSE_QUEUE_SIZE;
 //    	    }
 
 //    	    /* Else wait until queue has some space */
@@ -191,7 +191,7 @@ static int push_pop_response(struct skbuff_nic_c **skbuff_struct, int cpu, int i
 //    	    	return -2;
 //    	    }
 
-        temp_node = (struct queue_ll*) &response_queue[cpu][allocator[cpu]++];
+//        temp_node = (struct queue_ll*) &response_queue[cpu][allocator[cpu]++];
 
         /* skbuff needs to be add to link list */
         temp_node->skbuff_struct = *skbuff_struct;
