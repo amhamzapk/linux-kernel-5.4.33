3183ef136be207fcd3c520442a8fa283cdcd3957
Ameer Hamza <amhamza.pk@gmail.com>
2020-07-06 17:33:35 +0500

    q_mem_1 allocation by ptr

--- _CustomModules/c_model/c_model.c
+++ _CustomModules/c_model/c_model.c
@@ -347,14 +347,14 @@ static int c_model_worker_thread(void *unused) {
                         skbuff_ptr->meta.response_flag = CASE_NOTIFY_STACK_RX;
 
                         /* Pass skbuff to response queue */
-//                        push_pop_response(&skbuff_ptr, skbuff_ptr->meta.cpu, 1);
-                        if (push_pop_response(&skbuff_ptr, skbuff_ptr->meta.cpu, 1) == -2)
-                        {
-                        	do
-                        	{
-                        		udelay (10);
-                        	} while (push_pop_response(&skbuff_ptr, skbuff_ptr->meta.cpu, 1) == -2);
-                        }
+                        push_pop_response(&skbuff_ptr, skbuff_ptr->meta.cpu, 1);
+//                        if (push_pop_response(&skbuff_ptr, skbuff_ptr->meta.cpu, 1) == -2)
+//                        {
+//                        	do
+//                        	{
+//                        		udelay (10);
+//                        	} while (push_pop_response(&skbuff_ptr, skbuff_ptr->meta.cpu, 1) == -2);
+//                        }
 //                        set_current_state(TASK_INTERRUPTIBLE);
 //                        schedule_timeout (1);
 //                        flush_cache_all();
