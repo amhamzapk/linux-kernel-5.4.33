52cda886b374d2ac302cf42917115cdcd91ce215
Ameer Hamza <amhamza.pk@gmail.com>
2020-07-14 16:04:32 +0500

    MTU Update

--- _CustomModules/mlx_vf/mlx5/core/port.c
+++ _CustomModules/mlx_vf/mlx5/core/port.c
@@ -62,7 +62,14 @@ int mlx5_core_access_reg(struct mlx5_core_dev *dev, void *data_in,
 
 	err = mlx5_cmd_exec(dev, in, inlen, out, outlen);
 	if (err)
+	{
+		printk(KERN_ALERT "Error: Mtu Update\n");
+	}
+	else
+	{
+		printk(KERN_ALERT "No Error: Mtu Update\n");
 		goto out;
+	}
 
 	data = MLX5_ADDR_OF(access_register_out, out, register_data);
 	memcpy(data_out, data, size_out);
